ac3b519a6b43f98f3a97ee6dfe0af3fe6bce225e
tycalder <60930970+tycalder@users.noreply.github.com>
2020-02-25 10:50:05 -0500

    Add files via upload

---
+++ Classes/Ranking.py
@@ -0,0 +1,9 @@
+from Classes import Snippet 
+class Ranking:
+    """description of class"""
+    mostRecent: Snippet
+    oldest: Snippet 
+    current: Snippet
+
+
+
---
+++ Classes/Snippet.py
@@ -0,0 +1,7 @@
+class Snippet:
+    """simple class for Snippet"""
+    textFile: str
+    videoFile: str
+    Position: int
+
+
