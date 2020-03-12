665ef609d4bdf56c2785bf0a77ef833c140fcd58
tycalder <tycalder@gmail.com>
2020-03-12 08:42:03 -0400

    Update bitTest.py

--- bitTest.py
+++ bitTest.py
@@ -2,9 +2,10 @@ import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 21 to be an input pin and set initial value to be pulled low (off)
 
 while True: # Run forever
     if GPIO.input(21) == GPIO.HIGH:
         print("Bit detected")
-        
+        #stop running file processes
+        #save files to permanent directory
