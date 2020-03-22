703c5754e8bc81679f2865e2c4f085da3e17577f
tycalder <tycalder@gmail.com>
2020-03-22 15:22:10 -0400

    Update bitTest.py

--- bitTest.py
+++ bitTest.py
@@ -4,8 +4,13 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 21 to be an input pin and set initial value to be pulled low (off)
 
-while True: # Run forever
-    if GPIO.input(21) == GPIO.HIGH:
-        print("Bit detected")
+def crashDetected():
+    while True: # Run forever
+        if GPIO.input(21) == GPIO.HIGH:
+            print("Bit detected")
+            return True
+        else:
+            return False
+        
         #stop running file processes
         #save files to permanent directory
